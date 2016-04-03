e78fdc6348e34620d71c18ac0e48d126a9e188a2
Shiv Pratap Singh <shivthefearlessforever@gmail.com>
2016-04-03 18:06:35 +0530

    my website
    This is my website

---
+++ shiv_frontend/js/contact.js
@@ -0,0 +1,17 @@
+﻿ jQuery(function ($) {
+        function init_map1() {
+            var myLocation = new google.maps.LatLng(25.43, 81.7725);
+            var mapOptions = {
+                center: myLocation,
+                zoom: 16
+            };
+            var marker = new google.maps.Marker({
+                position: myLocation,
+                title: "Property Location"
+            });
+            var map = new google.maps.Map(document.getElementById("map1"),
+                mapOptions);
+            marker.setMap(map);
+        }
+        init_map1();
+    });
